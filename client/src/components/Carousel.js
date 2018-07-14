c27838b1a9061ea989e4254486d8a2b60c91d14f
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-07-14 13:08:32 -0400

    heroku deploy'

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -3,22 +3,22 @@ import React from "react";
 
 const Carousel = () => (
 <div>
-<div id="carouselExampleSlidesOnly" ClassName="carousel slide" data-ride="carousel">
-  <div ClassName="carousel-inner">
-    <div ClassName="carousel-item active">
-      <img ClassName="d-block w-100" src="" alt="First pic"/>
+<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiqqra_h5_cAhVOON8KHTGKD-sQjRx6BAgBEAU&url=https%3A%2F%2Feffortz.com%2Fproduct%2Fstandard-web-hosting-plan-on-dedicated-server%2F&psig=AOvVaw2ICvpO-N-3Uyfl9fyuKrYZ&ust=1531673747522407" alt="First pic"/>
     </div>
-    <div ClassName="carousel-item">
-      <img ClassName="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYIBsRd87rWxNz-j8KdOTJcfF3C4g5iZjigzIUNF7HH08RxuqZ" alt="Second pic"/>
+    <div className="carousel-item">
+      <img className="d-block w-100 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYIBsRd87rWxNz-j8KdOTJcfF3C4g5iZjigzIUNF7HH08RxuqZ" height='300' alt="Second pic"/>
     </div>
-    <div ClassName="carousel-item">
-      <img ClassName="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKv1wDXdaCBMH0FsBOFPksF10eWNtLko4FRTB7ygGraNXKT_s9Pw" alt="Third pic"/>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKv1wDXdaCBMH0FsBOFPksF10eWNtLko4FRTB7ygGraNXKT_s9Pw"  height='300' alt="Third pic"/>
     </div>
-    <div ClassName="carousel-item">
-      <img ClassName="d-block w-100" src="" alt="Fifth pic"/>
+    <div className="carousel-item">
+      <img className="d-block w-100 " src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmsdnshared.blob.core.windows.net%2Fmedia%2F2017%2F01%2Fservers.jpg&imgrefurl=https%3A%2F%2Fblogs.technet.microsoft.com%2Fuktechnet%2F2017%2F01%2F17%2Fhow-to-boost-your-windows-server-2016-security%2F&docid=pxvXnD-14XevRM&tbnid=7H2c8ObMFP-DvM%3A&vet=10ahUKEwjms5m-h5_cAhXjdd8KHcZ7BHwQMwigAigVMBU..i&w=900&h=350&bih=602&biw=1242&q=server&ved=0ahUKEwjms5m-h5_cAhXjdd8KHcZ7BHwQMwigAigVMBU&iact=mrc&uact=8" alt="Fifth pic"/>
     </div>
-    <div ClassName="carousel-item">
-      <img ClassName="d-block w-100" src="" alt="Fourth pic"/>
+    <div className="carousel-item">
+      <img className="d-block w-100 " src="https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.lenitytech.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fcomputer_networking.png&imgrefurl=http%3A%2F%2Fwww.lenitytech.com%2Fblog%2Feffective-management-of-complexities-in-the-field-of-internet-computer-networking%2F&docid=bmoAkXk0Hi4I4M&tbnid=Nhc2Skmkd1g_oM%3A&vet=10ahUKEwjRzIzmh5_cAhWkneAKHdeIAqMQMwjwASgGMAY..i&w=610&h=335&bih=602&biw=1242&q=networking&ved=0ahUKEwjRzIzmh5_cAhWkneAKHdeIAqMQMwjwASgGMAY&iact=mrc&uact=8" alt="Fourth pic"/>
     </div>
     </div>
   </div>
