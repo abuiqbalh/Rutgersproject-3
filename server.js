9bfe25eec0ff2b1720ab4cc8391ef84072b8f248
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-07-17 19:19:22 -0400

    heroku

--- server.js
+++ server.js
@@ -27,8 +27,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require("./route/authRoutes")(app);
-require('./route/htmlRoutes')(app);
-
+// require('./route/htmlRoutes')(app);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
