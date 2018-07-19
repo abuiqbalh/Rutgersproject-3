d058e09131e298fccab5107216b838206b40da83
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-07-18 20:13:40 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 
-module.exports = {
-  googleClientID:"401384376909-dpkgrtifgkqol0ua5i78roanju4r05k0.apps.googleusercontent.com",
-  googleClientSecret:"ESNv03JobhFJV1eIX1RVXLNG",
-  mongoURI:"mongodb://admin:flower.123@ds235850.mlab.com:35850/nodeserverudemy",
-  cookieKey:"kdjfkdjfdkjfi09u0qdfkdfkl45579"
-
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
 }
