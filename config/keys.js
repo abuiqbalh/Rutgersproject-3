5212f80a72040838223782e9ed5933900d1a7881
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-07-19 01:15:01 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,17 @@
 
-if (process.env.NODE_ENV === 'production') {
-  // we are in production - return the prod set of keys
-  module.exports = require('./prod');
-} else {
-  // we are in development - return the dev keys!!!
-  module.exports = require('./dev');
+module.exports = {
+  googleClientID:"401384376909-dpkgrtifgkqol0ua5i78roanju4r05k0.apps.googleusercontent.com",
+  googleClientSecret:"ESNv03JobhFJV1eIX1RVXLNG",
+  mongoURI:"mongodb://admin:flower.123@ds125821.mlab.com:25821/rutgersproject3",
+  cookieKey:"kdjfkdjfdkjfi09u0qdfkdfkl45579"
+
 }
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   // we are in production - return the prod set of keys
+//   module.exports = require('./prod');
+// } else {
+//   // we are in development - return the dev keys!!!
+//   module.exports = require('./dev');
+// }
